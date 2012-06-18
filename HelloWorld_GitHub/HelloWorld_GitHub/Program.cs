6732cb1d7e10fdaf1f5beaf5eb49a405df8a213c
Benson Chiu <bc1118@hotmail.com>
2012-06-17 18:09:56 -0700

    Test Commit a Project

---
+++ HelloWorld_GitHub/HelloWorld_GitHub/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld_GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
